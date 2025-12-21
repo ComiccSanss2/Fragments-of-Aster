0c64b37204a99ab9c225a8df190b495d93a806f4
Hamza Ben Alouane <114566360+ComiccSanss2@users.noreply.github.com>
2025-12-21 20:11:55 +0100

    some adjustments and fixes

--- loading_screen.gd
+++ loading_screen.gd
@@ -1,21 +1,26 @@
 extends Control
 
 var target_scene_path: String = ""
-var min_load_time: float = 3.0 
+var min_load_time: float = 5.0 
 var time_elapsed: float = 0.0
 var progress: Array = []
 
 @onready var sprite = $AnimatedSprite2D
 
 func _ready():
+	# Animation du Sprite
 	if sprite.sprite_frames.has_animation("run"):
 		sprite.play("run")
-	elif sprite.sprite_frames.has_animation("walk"):
-		sprite.play("walk")
+
+	
+	# --- 1. FADE IN (Apparition douce) ---
+	self.modulate.a = 0.0
+	var t = create_tween()
+	t.tween_property(self, "modulate:a", 1.0, 0.5)
+	# -------------------------------------
 	
 	if target_scene_path == "":
 		print("Erreur : Aucune scène cible définie !")
-		# On se détruit si pas de cible pour éviter de bloquer l'écran
 		queue_free() 
 		return
 		
@@ -23,16 +28,23 @@ func _ready():
 
 func _process(delta):
 	time_elapsed += delta
-	# Animation visuelle
+	
+	# Animation visuelle (Ping-Pong)
 	sprite.position.x += sin(time_elapsed * 10) * 0.5
 	
 	var status = ResourceLoader.load_threaded_get_status(target_scene_path, progress)
 	
 	if status == ResourceLoader.THREAD_LOAD_LOADED:
 		if time_elapsed >= min_load_time:
-			var new_scene = ResourceLoader.load_threaded_get(target_scene_path)
-			get_tree().change_scene_to_packed(new_scene)
-			
-			# --- LA CORRECTION EST ICI ---
-			queue_free() # On détruit l'écran de chargement pour voir le jeu !
-			# -----------------------------
+			finish_loading()
+
+func finish_loading():
+	# On arrête le process pour être sûr de ne pas le lancer deux fois
+	set_process(false)
+	
+	# 1. On change la scène
+	var new_scene = ResourceLoader.load_threaded_get(target_scene_path)
+	get_tree().change_scene_to_packed(new_scene)
+	
+	# On détruit l'écran de chargement immédiatement pour révéler le jeu d'un coup
+	queue_free()
