a6ca4a29678292a659ca6cb70924876a10c4a06f
Hamza Ben Alouane <114566360+ComiccSanss2@users.noreply.github.com>
2026-02-26 16:36:58 +0100

    improved glitch wall chase

--- glitch_wall.gd
+++ glitch_wall.gd
@@ -1,16 +1,30 @@
 extends Area2D
 
-# Vitesse du mur (à ajuster selon la vitesse de course du joueur)
-@export var speed := 150.0 
+# Vitesse de base du mur
+@export var speed := 240.0 
+@export var boost_speed := 600.0
+@export var max_distance := 700.0
+@export var safe_distance := 400.0
+@export var smoothness := 2.0
+
+var current_speed := 240.0
 
 func _physics_process(delta):
-	# Le mur avance toujours vers la droite
-	position.x += speed * delta
+	var player = get_tree().get_first_node_in_group("player")
+	var target_speed = speed 
+	
+	if player:
+		var distance = player.global_position.x - global_position.x
+		
+		if distance > max_distance:
+			target_speed = boost_speed
 
-# Connecte le signal "body_entered" de l'Area2D à ce script
+	current_speed = lerp(current_speed, target_speed, smoothness * delta)
+	
+	# Application du mouvement
+	position.x += current_speed * delta
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		# Au lieu d'appeler le Main directement, on appelle la vraie mort du joueur !
-		# On lui passe global_position pour que le joueur soit repoussé dans le bon sens.
 		if body.has_method("die"):
 			body.die(global_position)
